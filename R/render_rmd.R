a8c83217aff8f0cf6669123c1ce373cfcae70762
chinhungtseng <bluesshown@gmail.com>
2019-05-25 17:21:23 +0800

    test

--- R/render_rmd.R
+++ R/render_rmd.R
@@ -2,4 +2,4 @@
 files <- list.files(pattern = "[.]Rmd$")
 
 # render all the .Rmd file
-for (f in files) {rmarkdown::render(f, "html_document", output_dir = "_doc/")}
+for (f in files) {rmarkdown::render(f, "prettydoc::html_pretty", output_dir = "_doc/")}
